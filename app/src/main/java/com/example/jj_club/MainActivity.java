6ff5e22cad0e54b6eb0544f67547d0a3382660ee
minji9988 <rnwjf111@daum.net>
2023-05-14 21:44:58 +0900

    LoginActivity

--- app/src/main/java/com/example/jj_club/MainActivity.java
+++ app/src/main/java/com/example/jj_club/MainActivity.java
@@ -1,22 +1,33 @@
-////////////////////로그인 페이지///////////////////
 package com.example.jj_club;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jj_club.databinding.ActivityMainBinding;
 
-// 로그인 페이지
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-
-
+//////////// 로그인 페이지///////////////
 
 public
 class MainActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    private LoginInterface loginInterface;
 
     @Override
     protected
@@ -24,10 +35,60 @@ class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ////////////////////////로그인 > 회원가입 페이지(이메일 인증)로 이동 Start ///////////////////////
+        emailEditText = findViewById(R.id.editText_email_login);
+        passwordEditText = findViewById(R.id.editText_password);
+        loginButton = findViewById(R.id.button_login);
 
-        // 로그인 페이지(activity_main.xml)에 있는 '회원가입하러가기' 텍스트 클릭 시
-        // RegisterActivity(회원가입 페이지)로 이동
+        loginInterface = RetrofitClient.getClient().create(LoginInterface.class);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    emailEditText.setHintTextColor(Color.RED);
+                    Toast.makeText(MainActivity.this, "이메일을 입력하세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    passwordEditText.setHintTextColor(Color.RED);
+                    Toast.makeText(MainActivity.this, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Call<LoginResponse> call = loginInterface.login(email, password);
+                call.enqueue(new Callback<LoginResponse>() {
+                    @Override
+                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                        if (response.isSuccessful()) {
+                            LoginResponse loginResponse = response.body();
+                            if (loginResponse != null && loginResponse.isSuccess()) {
+                                Toast.makeText(MainActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                                // 로그인 성공 시 MainPageActivity로 화면 전환
+                                Intent intent = new Intent(MainActivity.this, MainPageActivity.class);
+                                startActivity(intent);
+                                finish(); // 현재 액티비티 종료
+                            } else {
+                                Toast.makeText(MainActivity.this, "이메일 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "로그인 요청 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResponse> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "로그인 요청 실패: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        // 로그인 페이지에 있는 '회원가입하러가기' 텍스트 클릭 시
+        // 회원가입 페이지(이메일 인증)로 이동
         TextView txtRegister = (TextView) findViewById(R.id.registerTextButton);
         txtRegister.setOnClickListener(new View.OnClickListener() {
             public void onClick( View view) {
@@ -37,8 +98,10 @@ class MainActivity extends AppCompatActivity {
             }
         });
 
-        ////////////////////////로그인 > 회원가입 페이지(이메일 인증)로 이동 END ///////////////////////
 
     }
 
 }
+
+
+

cc04f399692a70dfe57215806ca140d4c4ab5644
wlgur313 <wlgur313@naver.com>
2023-05-21 21:41:47 +0900

    promotion

---
+++ app/src/main/java/com/example/jj_club/PromotionWrite1.java
@@ -0,0 +1,14 @@
+package com.example.jj_club;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PromotionWrite1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promotion_write1);
+    }
+}
